bc51c26a74317f5378d188a2f26f1877fed9fbf3
joan-ch-18 <joanchindoyh@gmail.com>
2025-02-25 11:07:02 -0500

    Mensaje de Prueba

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hola, estoy probando");
         System.out.println("Te amo Wilfer");
+        System.out.println("Probando ando");
 
     }
 }
